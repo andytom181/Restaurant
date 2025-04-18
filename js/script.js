23efe4298b72c5fe0211db6185e946f0c67e9de4
andytom181 <63881084+andytom181@users.noreply.github.com>
2025-04-19 01:05:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// You can add interactivity here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website Loaded');
+});
+  
+document.getElementById("foodMenu").onclick = function () {
+  location.href = "/food.html";
+};
+
+fetch("/navbar.html")
+.then(response => response.text())
+.then(data => {
+  document.getElementById("navbar-placeholder").innerHTML = data;
+});
